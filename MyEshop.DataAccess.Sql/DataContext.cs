11c249ac804310b8e0e859d2a3e0840b978295aa
Eddygaras <edgaras.smirnovas@gmail.com>
2018-03-21 10:21:37 +0000

    created link to sql and created tables automatically

---
+++ MyEshop.DataAccess.Sql/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using MyEshop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEshop.DataAccess.Sql
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> productCategories { get; set; }
+            
+        
+
+
+
+
+    }
+}
---
+++ MyEshop.DataAccess.Sql/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace MyEshop.DataAccess.Sql.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyEshop.DataAccess.Sql.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MyEshop.DataAccess.Sql.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
