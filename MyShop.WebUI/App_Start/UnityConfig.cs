4d579b28b5ff21d44d17844503b9ecbec3708526
Eddygaras <edgaras.smirnovas@gmail.com>
2018-03-21 12:01:12 +0000

    Created SQL Repository that does insert and edit, delete functions on sql server tables

--- MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using MyEshop.Core.Contracts;
 using MyEshop.Core.Models;
 using MyEshop.DataAccess.InMemory;
+using MyEshop.DataAccess.Sql;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace MyShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
 
         }
     }
